8cbdc80f53d8ad0f7a3eac10d842d3530197c5c2
albertdc1984 <92040589+albertdc1984@users.noreply.github.com>
2022-09-07 23:43:57 +0200

    Added difficulty button and state change

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import anarchy from "./android-icon-192x192.png";
 import "./App.css";
 import Button from "./components/Button";
@@ -6,10 +7,15 @@ import GameScreen from "./components/GameScreen";
 import MainScreen from "./components/MainScreen";
 
 function App() {
+  const [difficulty, setDifficulty] = useState(0);
+
+  const changeDifficulty = () => {
+    setDifficulty(difficulty === 2 ? 0 : difficulty + 1);
+  };
   return (
     <div className="App">
       <img src={anarchy} className="App-logo" alt="logo" />
-      <MainScreen />
+      <MainScreen difficulty={difficulty} changeDifficulty={changeDifficulty} />
       <GameScreen />
       <FinishScreen />
       <Button text="Hola" action={() => console.log("que tal?")} />
--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -1,9 +1,35 @@
+import styled from "styled-components";
 import Button from "./Button";
 
+const TitleBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const ButtonBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
 export default function MainScreen(props) {
+  const difficultyLevels = ["Easy", "Medium", "Hard"];
   return (
     <div>
-      <Button text="Start" action={() => console.log("Starting...")} />
+      <TitleBox>
+        <h1>Memo Game</h1>
+        <h2>Choose difficulty</h2>
+      </TitleBox>
+      <ButtonBox>
+        <Button
+          text={difficultyLevels[props.difficulty]}
+          action={props.changeDifficulty}
+        />
+        <br />
+        <Button text="Start" action={() => console.log("Starting...")} />
+      </ButtonBox>
     </div>
   );
 }
