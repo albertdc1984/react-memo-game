b3266cc6cb455a3c38b049301248fbea99f292a1
albertdc1984 <92040589+albertdc1984@users.noreply.github.com>
2022-09-08 23:45:38 +0200

    Random array generator created

---
+++ src/logic/crreateRandomCardsArray.js
@@ -0,0 +1,32 @@
+import cardObject from "./cards";
+
+const randomCardsArr = (numbCards) => {
+  const halfCards = numbCards / 2;
+  const arr = [];
+  let i = 0,
+    j = 0;
+
+  while (i < numbCards) {
+    if (j === halfCards) {
+      j = 0;
+    }
+    let random = Math.floor(Math.random() * numbCards);
+
+    if (!arr.some((element) => element.id === random)) {
+      arr.push({
+        id: random,
+        card: cardObject[j],
+        bind: j,
+        rotate: false,
+        valid: 0,
+        fixed: 0,
+      });
+
+      i++;
+      j++;
+    }
+  }
+  return arr;
+};
+
+export default randomCardsArr;
